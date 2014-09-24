6082a056f89380076633905dbcf9c5c026ab0f04
Milo Thurston <knirirr@gmail.com>
2014-09-24 13:17:06 +0100

    Now creates both metadata and data.

--- scraper.py
+++ scraper.py
@@ -12,12 +12,23 @@ import urllib
 import json
 import pprint
 
-
+create_package = 'http://tesstest2.oerc.ox.ac.uk/api/action/package_create'
+create_resource = 'http://tesstest2.oerc.ox.ac.uk/api/action/resource_create'
 root_url = 'http://software-carpentry.org/'
-page = requests.get(root_url + 'lessons.html')
-tree = html.fromstring(page.text)
 
-def get_data():
+
+def get_metadata():
+    return {
+        'name' : 'software_carpentry',
+        'title': 'Software carpentry courses',
+        'notes' : 'Uploaded via CKAN API'
+    }
+
+# There doesn't seem to be much need to parse this at the moment - CKAN seems to be expecting the URL of a resource
+# rather than the resource itself.
+def parse_data():
+    page = requests.get(root_url + 'lessons.html')
+    tree = html.fromstring(page.text)
     links = tree.xpath('//ul/li/a')
     lessons = {}
     for item in links:
@@ -30,12 +41,31 @@ def get_data():
 
     return lessons
 
-def get_test_data():
+def get_data():
+    return {
+        'package_id' : get_metadata()['name'],
+        'url': root_url + 'lessons.html',
+        'description': 'Some lessons in how to write code &c.',
+        'name': 'Lessons',
+        'format': 'text/html'
+    }
+
+def get_test_metadata():
     return {
         'name' : 'wibble',
         'title': 'Some Exciting Example Data',
         'notes' : 'Uploaded via CKAN API'
     }
+
+def get_test_data():
+    return {
+        'package_id' : get_test_metadata()['name'],
+        'url': 'http://tesstest2.oerc.ox.ac.uk/dataset/' + get_test_metadata()['name'],
+        'description': 'Nothing to see here, move along...',
+        'name': 'Some test data',
+        'format': 'text/plain'
+    }
+
 
 def print_data(data):
     for key in data:
@@ -43,7 +73,7 @@ def print_data(data):
 
 # This won't work - we'll have to create the new dataset (as in get_test_data()) and then
 # add data to it somehow
-def upload_data(data):
+def create_data(data,url):
     # process data to json for uploading
     data_string = urllib.quote(json.dumps(data))
     pprint.pprint(data_string)
@@ -51,7 +81,7 @@ def upload_data(data):
     # get the api key for authorisation
     with open ("api.txt", "r") as apifile:
         api = apifile.read().replace('\n', '')
-    request = urllib2.Request('http://tesstest2.oerc.ox.ac.uk/api/action/package_create')
+    request = urllib2.Request(url)
     request.add_header('Authorization', api)
 
     # Make the HTTP request - check the apache logs to see the reason for any crashes
@@ -68,5 +98,6 @@ def upload_data(data):
 
 
 
-upload_data(get_test_data())
+#create_data(get_metadata(),create_package)
+create_data(get_data(),create_resource)
 
