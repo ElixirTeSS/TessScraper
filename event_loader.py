2546ad79c88c5c52e1a82f50005b492274ddc01f
Milo Thurston <knirirr@gmail.com>
2015-01-23 09:37:17 +0000

    Writes html file for reading on the tess server.

--- event_loader.py
+++ event_loader.py
@@ -3,20 +3,42 @@ __author__ = 'milo'
 import urllib2
 import json
 import pprint
+import datetime
+from dateutil import parser
 
-events_url = 'http://iann.pro/solr/select/?q=*:*&rows=4&fl=start,title,country,link&fq=end%3A[NOW%20TO%20*]&wt=json'
+def get_stuff():
+    events_url = 'http://iann.pro/solr/select/?q=*:*&rows=4&fl=start,title,country,link&fq=end%3A[NOW%20TO%20*]&wt=json'
 
-response = urllib2.urlopen(events_url)
-data = json.loads(response.read())
+    response = urllib2.urlopen(events_url)
+    data = json.loads(response.read())
 
-pprint.pprint(data)
+    pprint.pprint(data)
 
-# Save the data as a file for loading by ckan
-with open('data.json', 'w') as outfile:
-    json.dump(data, outfile)
+    # Save the data as a file for loading by ckan
+    #with open('data.json', 'w') as outfile:dt = parser.parse(entry['start'])
+    #    json.dump(data, outfile)
+    string = "<ul>\n"
+    f = open('events.txt','w')
+    for entry in data['response']['docs']:
+        dt = parser.parse(entry['start'])
+        string += "<li>"
+        string +="<a href=\"" + entry['link'] + "\">" + entry['title'] + "</a>\n"
+        string += "<br>" + dt.strftime('%A %B %d, %Y') + " &mdash; " +  entry['country']
+        string += "</li>\n"
+    string += "</ul>\n"
+    f.write(string) # python will convert \n to os.linesep
+    f.close()
 
 
-# To load this json from a file
-#with open('data.json') as data_file:
-#   data = json.load(data_file)
-#   pprint.pprint(data)
+def read_stuff():
+    # To load this json from a file
+    with open('data.json') as data_file:
+        #data = json.load(data_file)
+        #pprint.pprint(data)
+        data = data_file.read()
+        return data
+
+
+get_stuff()
+#result = read_stuff()
+#print result
